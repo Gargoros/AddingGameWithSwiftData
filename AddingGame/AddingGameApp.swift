a500cf63ade24b241f19238eee3f0b54fbb2da44
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2024-02-05 09:55:52 +0300

    Initial Commit

---
+++ AddingGame/AddingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddingGameApp.swift
+//  AddingGame
+//
+//  Created by MIKHAIL ZHACHKO on 5.02.24.
+//
+
+import SwiftUI
+
+@main
+struct AddingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
